8460c7c77407790a9aaf952b1f58128802cd7ab3
DS <derekstander@gmail.com>
2021-09-11 14:32:16 -0400

    modified:   server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: process.env.SECRET,
+  secret: 'SuP3rT3rc3s',
   cookie: {},
   resave: false,
   saveUninitialized: true,
